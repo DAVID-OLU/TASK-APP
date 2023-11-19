16723b2a36e7b951e219de8357b9fc1f400e3cea
David <ogunsanwodavid0@gmail.com>
2023-11-18 21:43:04 -0800

    INSTALLED MYSQL

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -86,23 +86,13 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#          'USER': 'root@localhost',
-#          'PASSWORD': 'PASSWORD', 
-
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/my.cnf',
-        },
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+         'NAME': BASE_DIR / 'db.sqlite3',
+         'USER': 'root@localhost',
+         'PASSWORD': 'john@1HACKER', 
+
     }
 }
 # import dj_database_url
