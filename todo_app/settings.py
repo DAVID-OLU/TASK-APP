ee8b22f3f7ec9e0cc4e1e9a1b7733b3a2ec30530
David <ogunsanwodavid0@gmail.com>
2023-11-18 21:47:32 -0800

    INSTALLED MYSQL

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['david-todo-app-cb7d15d76a9d.herokuapp.com']
 
