40cb6d03450a39767105041da091b2abb176e5b9
David ogunsanwo <oriyomijhn@gmail.com>
2023-12-18 10:05:48 +0300

    added screte key

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = os.environ['MY_KEY']
+MY_KEY = os.environ['MY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
